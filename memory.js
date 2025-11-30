0ee4423f3d3b9f618a42cd721ab6863c2c2864fe
Vikas Yadav <vikasvy369@gmail.com>
2025-11-30 23:28:01 +0530

    upated

--- memory.js
+++ memory.js
@@ -4,19 +4,20 @@
 // let anotherName = myHomeName
 // anotherName = "Vikas"
 // console.log(myHomeName)
-console.log(anotherName)
+// console.log(anotherName)
 
-let userOne={
-    email:"vikas@gmail.com",
-    upi: "vikas@ybl"
-}
-let userTwo= userOne
+// let userOne={
+//     email:"vikas@gmail.com",
+//     upi: "vikas@ybl"
+// }
+// let userTwo= userOne
 
-userTwo.email = "Xikas@google.com"
-console.log(userOne.email)
-console.log(userTwo.email)
+// userTwo.email = "Xikas@google.com"
+// console.log(userOne.email)
+// console.log(userTwo.email)
 
-let  arr1 = [1,2,4,5,6];
-let arr2 = [...arr1];   //spread
-arr1.push(8)
-console.log(arr2)
+// let  arr1 = [1,2,4,5,6];
+// let arr2 = [...arr1];   //spread
+// arr1.push(8)
+// console.log(arr2)
+//tomorrow i will lern js again 
